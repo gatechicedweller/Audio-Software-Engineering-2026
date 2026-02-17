41b7831381cb2ab8af061f2da37e3d885b2c50d6
Noel Alben <65276123+nol-alb@users.noreply.github.com>
2026-02-17 14:09:58 -0500

    CombFilter Assignment Base
    Base Template Implementation For CombFilter Assignment

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -1,51 +1,168 @@
 
+#include <iostream>
 #include <ctime>
 
 #include "MUSI6106Config.h"
 
-#include <iostream>
-#include "RingBuffer.h"
+#include "AudioFileIf.h"
+#include "CombFilterIf.h"
 
 using std::cout;
 using std::endl;
 
 // local function declarations
 void    showClInfo ();
+void test();
 
 /////////////////////////////////////////////////////////////////////////////////
 // main function
 int main(int argc, char* argv[])
 {
+    std::string             sInputFilePath,                 //!< file paths
+                            sOutputFilePath;
+
+    static const int        kBlockSize = 1024;
+
+    clock_t                 time = 0;
+
+    float                   **ppfAudioData = 0;
+
+    CAudioFileIf            *phAudioFile = 0;
+    std::fstream            hOutputFile;
+    CAudioFileIf::FileSpec_t stFileSpec;
+
+    // Initialize CombFilter
+
     showClInfo();
 
-    CRingBuffer<float>* pCRingBuff = 0;
-    static const int kBlockSize = 17;
-    pCRingBuff = new CRingBuffer<float>(kBlockSize);
-
-    // above can be replaced by: auto* pCRingBuff = new CRingBuffer<float>(kBlockSize);
-
-    for (int i = 0; i < 5; i++)
+    //////////////////////////////////////////////////////////////////////////////
+    // parse command line arguments
+    // If no arguments, run tests
+    if (argc < 2)
     {
-        pCRingBuff->putPostInc(1.F*i);
+        cout << "Missing audio input path! -- Running Tests now" << endl;
+        test();
+        return -1;
+    }
+    // else check and assign filter parameters
+    else
+    {
+        sInputFilePath = argv[1];
+        sOutputFilePath = sInputFilePath + ".txt";
     }
 
-    float sample = 0.0F;
-    for (int i = 5; i < 30; i++)
+    //////////////////////////////////////////////////////////////////////////////
+    // open the input wave file
+    CAudioFileIf::create(phAudioFile);
+    phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead);
+    if (!phAudioFile->isOpen())
     {
-        pCRingBuff->getNumValuesInBuffer(); // should be five
-        sample = pCRingBuff->getPostInc(); // should be i-5
-        pCRingBuff->putPostInc(1.F*i);
+        cout << "Wave file open error!";
+        CAudioFileIf::destroy(phAudioFile);
+        return -1;
+    }
+    phAudioFile->getFileSpec(stFileSpec);
+
+    //////////////////////////////////////////////////////////////////////////////
+    // open the output text file
+    hOutputFile.open(sOutputFilePath.c_str(), std::ios::out);
+    if (!hOutputFile.is_open())
+    {
+        cout << "Text file open error!";
+        CAudioFileIf::destroy(phAudioFile);
+        return -1;
     }
 
-    cout << sample << endl;
+    //////////////////////////////////////////////////////////////////////////////
+    // allocate memory
+    ppfAudioData = new float*[stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        ppfAudioData[i] = new float[kBlockSize];
+
+    if (ppfAudioData == 0)
+    {
+        CAudioFileIf::destroy(phAudioFile);
+        hOutputFile.close();
+        return -1;
+    }
+    if (ppfAudioData[0] == 0)
+    {
+        CAudioFileIf::destroy(phAudioFile);
+        hOutputFile.close();
+        return -1;
+    }
+    /////////////////////////////////////////////////////
+    // Allocate memory for the CombFilter class and initialize CombFilter
+
+
+    time = clock();
+
+    //////////////////////////////////////////////////////////////////////////////
+    // get audio data and write it to the output text file (one column per channel)
+    // apply comb filter
+    while (!phAudioFile->isEof()) {
+        // set block length variable
+        long long iNumFrames = kBlockSize;
+
+        // read data (iNumOfFrames might be updated!)
+        phAudioFile->readData(ppfAudioData, iNumFrames);
+
+        cout << "\r" << "reading and writing";
+
+        // write
+        for (int i = 0; i < iNumFrames; i++)
+        {
+            for (int c = 0; c < stFileSpec.iNumChannels; c++)
+            {
+                hOutputFile << ppfAudioData[c][i] << "\t";
+            }
+            hOutputFile << endl;
+        }
+    }
+
+    long long iNumFrames = kBlockSize;
+    phAudioFile->readData(ppfAudioData, iNumFrames);
+
+    cout << "\nreading/writing done in: \t" << (clock() - time) * 1.F / CLOCKS_PER_SEC << " seconds." << endl;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // clean-up (close files and free memory)
+    CAudioFileIf::destroy(phAudioFile);
+    hOutputFile.close();
+
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        delete[] ppfAudioData[i];
+    delete[] ppfAudioData;
+    ppfAudioData = 0;
+
     // all done
-    delete pCRingBuff;
-    pCRingBuff = nullptr;
-
     return 0;
 
 }
 
+// Write your tests here
+void test()
+{
+    cout << "\n---CombFilter Interface Testing ---\n";
+
+    CCombFilterIf* pFilter = nullptr;
+
+    // CREATE
+    Error_t err = CCombFilterIf::create(pFilter);
+
+    if (err == Error_t::kNoError)
+        cout << "create(): returned kNoError\n";
+    else
+        cout << "create(): returned error\n";
+
+    err = CCombFilterIf::destroy(pFilter);
+    if (err == Error_t::kNoError)
+        cout << "destroy(): returned kNoError\n";
+    else
+        cout << "destroy(): returned error\n";
+
+    cout << "=== Test Finished ===\n\n";
+}
 
 void     showClInfo()
 {
