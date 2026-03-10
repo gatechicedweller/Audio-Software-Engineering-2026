23218f434e77e4c2843fc06204ce1309e2d70f4b
Noel Alben <65276123+nol-alb@users.noreply.github.com>
2026-03-10 15:03:05 -0400

    Assignment 2 Template
    - Still need to edit the GTEST build framework

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -1,11 +1,9 @@
-
+#include <iostream>
 #include <ctime>
 
 #include "MUSI6106Config.h"
 
-#include <iostream>
-#include "RingBuffer.h"
-
+#include "AudioFileIf.h"
 using std::cout;
 using std::endl;
 
@@ -18,30 +16,8 @@ int main(int argc, char* argv[])
 {
     showClInfo();
 
-    CRingBuffer<float>* pCRingBuff = 0;
-    static const int kBlockSize = 17;
-    pCRingBuff = new CRingBuffer<float>(kBlockSize);
 
-    // above can be replaced by: auto* pCRingBuff = new CRingBuffer<float>(kBlockSize);
-
-    for (int i = 0; i < 5; i++)
-    {
-        pCRingBuff->putPostInc(1.F*i);
-    }
-
-    float sample = 0.0F;
-    for (int i = 5; i < 30; i++)
-    {
-        pCRingBuff->getNumValuesInBuffer(); // should be five
-        sample = pCRingBuff->getPostInc(); // should be i-5
-        pCRingBuff->putPostInc(1.F*i);
-    }
-
-    cout << sample << endl;
     // all done
-    delete pCRingBuff;
-    pCRingBuff = nullptr;
-
     return 0;
 
 }
