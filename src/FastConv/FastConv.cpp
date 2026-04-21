0113e03abaff6e935a17123932c3d142a020f126
gatechicedweller <gbarbieri6@gatech.edu>
2026-04-21 13:56:16 -0400

    started on flushbuffer logic

--- src/FastConv/FastConv.cpp
+++ src/FastConv/FastConv.cpp
@@ -13,14 +13,23 @@ CFastConv::~CFastConv( void ) //destroyer
 
 Error_t CFastConv::init(float *pfImpulseResponse, int iLengthOfIr, int iBlockLength /*= 8192*/, ConvCompMode_t eCompMode /*= kFreqDomain*/)
 {
-//I have to make a copy of ImpulseResponse cuz it's a pointer, everything else is free
+//I have to make my own copies of these inputs
 
     m_pfImpulseResponse = new float[iLengthOfIr];
     std::copy(pfImpulseResponse, pfImpulseResponse + iLengthOfIr, m_pfImpulseResponse);
 
+    //whole new buffer that holds the entire blocksize + ir length - 1
+    m_pfConvolutionBuffer = new float[iBlockLength + iLengthOfIr - 1];
+
+    // new output buffer of input length
     m_pfOutputBuffer = new float[iBlockLength]; 
 
-//find input buffer somewhere in here
+    //ACTUAL PROCESSING SIZE
+    m_iLengthOfIr = iLengthOfIr;
+
+    //EXPECTED PROCESSING SIZE
+    m_iBlockLength = iBlockLength;
+
     return Error_t::kNoError;
 }
 
@@ -31,32 +40,43 @@ Error_t CFastConv::reset()
         delete[] m_pfImpulseResponse;
         m_pfImpulseResponse = nullptr;
     }
-    
+
     if (m_pfOutputBuffer)
     {
         delete[] m_pfOutputBuffer;
         m_pfOutputBuffer = nullptr;
     }
+
+    if (m_pfConvolutionBuffer)
+    {
+        delete[] m_pfConvolutionBuffer;
+        m_pfConvolutionBuffer = nullptr;
+    }
+
     return Error_t::kNoError;
 }
 
 Error_t CFastConv::process (float* pfOutputBuffer, const float *pfInputBuffer, int iLengthOfBuffers )
 {
+     m_pfInputBuffer = new float[iLengthOfBuffers];
+    std::copy(pfInputBuffer, pfInputBuffer + iLengthOfBuffers, m_pfInputBuffer);
     if (m_eCompMode == kTimeDomain)
     {
         // do time domain convolution here
         for (int i = 0; i< iLengthOfBuffers; i++)
         {
-            pfOutputBuffer[i] = 0;
-            for (int j = 0; j < iLengthOfIr; j++)
+            m_pfConvolutionBuffer[i] = 0;
+            for (int j = 0; j < m_iLengthOfIr; j++)
             {
                 if (i-j >= 0)
                 {
-                    pfOutputBuffer[i] += m_pfImpulseResponse[iLengthOfIr-1-j] * m_pfInputBuffer[i-j];
+                    m_pfConvolutionBuffer[i] += m_pfImpulseResponse[m_iLengthOfIr-1-j] * m_pfInputBuffer[i-j];
                 }
-                
             }
         }
+
+        std::copy(m_pfConvolutionBuffer, m_pfConvolutionBuffer + iLengthOfBuffers, pfOutputBuffer);
+
     }
     else
     {
@@ -67,6 +87,7 @@ Error_t CFastConv::process (float* pfOutputBuffer, const float *pfInputBuffer, i
 
 Error_t CFastConv::flushBuffer(float* pfOutputBuffer)
 {
+    
     return Error_t::kNoError;
 }
 
